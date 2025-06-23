39f70006c714d19dfa5139c5050258eda76eca59
janezlapajne <lapajne.janez@gmail.com>
2025-06-23 15:42:55 +0200

    Refactor point extraction and transformation matrix calculation; enhance configuration handling and logging

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import typer
 from PIL import Image
+from siapy.entities.pixels import Pixels
+from siapy.transformations import corregistrator
 from siapy.utils.plots import pixels_select_click
 
-from src.core.configs import DATA_DIR, DATA_PROCESSED_DIR
+from src.core import configs
 from src.core.logger import setup_logger
 
 app = typer.Typer()
@@ -11,25 +13,30 @@ logger = setup_logger()
 
 
 @app.command()
-def extract_points(experiment_name: str):
-    experiment_path = DATA_DIR / experiment_name
-
-    if not experiment_path.exists():
-        logger.error(f"Experiment path {experiment_path} does not exist.")
-        raise typer.Exit(code=1)
-
+def extract_points(experiment_name: str, idx_start: int = 0):
+    experiment_path = configs.get_experiment_path(experiment_name)
     logger.info(f"Extracting points from experiment: {experiment_name}")
-    for img_path in experiment_path.glob("*.jpg"):
+
+    for img_path in sorted(experiment_path.glob("*.jpg"))[idx_start:]:
         logger.info(f"Processing image: {img_path.name}")
 
         img = Image.open(img_path)
         pixels = pixels_select_click(img)
-        pass
+        pixels.save_to_parquet(configs.get_points_path(experiment_name, img_path.stem))
 
 
 @app.command()
-def calculate_transformation_matrix():
-    pass
+def calculate_transformation_matrix(experiment_name: str):
+    points_path = configs.get_points_path(experiment_name)
+    logger.info(f"Calculating transformation matrix for experiment: {experiment_name}")
+
+    pixels_list = []
+    for points_path in sorted(points_path.glob("*.parquet")):
+        logger.info(f"Processing points file: {points_path.name}")
+        pixels = Pixels.load_from_parquet(points_path)
+        pixels_list.append(pixels)
+
+    matx, _ = corregistrator.align(pixels_list[0], pixels_list[1], plot_progress=False)
 
 
 if __name__ == "__main__":
--- src/core/configs.py
+++ src/core/configs.py
@@ -1,5 +1,12 @@
 import os
 from pathlib import Path
+from typing import Optional
+
+import typer
+
+from src.core.logger import setup_logger
+
+logger = setup_logger()
 
 BASE_DIR = Path(__file__).parent.parent.parent.absolute()
 DATA_DIR = BASE_DIR / "data"
@@ -7,3 +14,23 @@ DATA_PROCESSED_DIR = DATA_DIR / "processed"
 
 os.makedirs(DATA_DIR, exist_ok=True)
 os.makedirs(DATA_PROCESSED_DIR, exist_ok=True)
+
+
+def get_experiment_path(experiment_name: str) -> Path:
+    experiment_path = DATA_DIR / experiment_name
+    if not experiment_path.exists():
+        logger.error(f"Experiment path {experiment_path} does not exist.")
+        raise typer.Exit(code=1)
+    return experiment_path
+
+
+def get_points_path(experiment_name: str, image_name: Optional[str] = None) -> Path:
+    if not image_name:
+        points_file = DATA_PROCESSED_DIR / "points"
+        os.makedirs(points_file, exist_ok=True)
+    else:
+        points_file = (
+            DATA_PROCESSED_DIR / "points" / f"{experiment_name}_{image_name}.parquet"
+        )
+        os.makedirs(points_file.parent, exist_ok=True)
+    return points_file
